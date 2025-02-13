fe72e8952c81375fbbb8964524a5bb1a2891c9c8
poppy-lee <ppoblee@gmail.com>
2025-02-13 16:00:21 +0900

    move Button component to components/ToggleButton.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import {
-  CSSProperties,
-  startTransition,
-  useState,
-  unstable_ViewTransition as ViewTransition,
-} from "react";
+import { CSSProperties, useState } from "react";
+
+import { ToggleButton } from "./components/ToggleButton";
 
 export const App = () => {
+  const [checked, setChecekd] = useState(true);
+
   return (
     <div style={appContainerStyle}>
-      <Button />
+      <ToggleButton checked={checked} onChange={setChecekd} />
     </div>
   );
 };
@@ -18,54 +17,4 @@ const appContainerStyle: CSSProperties = {
   flex: 1,
   alignItems: "center",
   justifyContent: "center",
-};
-
-function Button() {
-  const [isOn, setIsOn] = useState(false);
-
-  const handleClickButton = () => {
-    startTransition(() => {
-      setIsOn(!isOn);
-    });
-  };
-
-  return (
-    <ViewTransition>
-      <button
-        style={{
-          ...buttonContainerStyle,
-          justifyContent: "flex-" + (isOn ? "end" : "start"),
-          backgroundColor: isOn
-            ? "rgba(0, 255, 0, 0.15)"
-            : "rgba(0, 0, 0, 0.1)",
-        }}
-        onClick={handleClickButton}
-      >
-        <ViewTransition>
-          <div
-            style={{
-              ...buttonHandleStyle,
-              backgroundColor: isOn ? "green" : "black",
-            }}
-          />
-        </ViewTransition>
-      </button>
-    </ViewTransition>
-  );
-}
-const buttonContainerStyle: CSSProperties = {
-  display: "flex",
-  width: 100,
-  height: 50,
-  padding: 10,
-  border: "none",
-  borderRadius: 50,
-  alignItems: "center",
-  cursor: "pointer",
-};
-const buttonHandleStyle: CSSProperties = {
-  width: 30,
-  height: 30,
-  backgroundColor: "black",
-  borderRadius: "50%",
 };
