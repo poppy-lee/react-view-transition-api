93dfa406701485c2bc3b4cbc82c0bb586b42f113
poppy-lee <ppoblee@gmail.com>
2025-02-14 13:32:46 +0900

    divide example components in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,33 @@
-import { useState, startTransition, CSSProperties } from "react";
+import { useState, startTransition, CSSProperties, ReactNode } from "react";
 
 import { Slider } from "../components/Slider";
 import { ToggleButton } from "../components/ToggleButton";
 import { Link } from "react-router";
 
 export function Home() {
+  return (
+    <div style={homeContainerStyle}>
+      <ToggleButtonExample />
+      <SliderExample />
+      <PageTransitionExample />
+    </div>
+  );
+}
+const homeContainerStyle: CSSProperties = {
+  display: "flex",
+  flex: 1,
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  overflow: "hidden",
+  gap: 40,
+};
+
+function ToggleButtonExample() {
   const [checked, setChecekd] = useState(true);
 
   return (
-    <div style={homeContainerStyle}>
+    <Example title="ToggleButtonExample">
       <label>
         <input
           type="checkbox"
@@ -32,17 +51,39 @@ export function Home() {
         external state change with startTransition()
       </label>
       <ToggleButton checked={checked} onChange={setChecekd} />
+    </Example>
+  );
+}
+function SliderExample() {
+  return (
+    <Example title="SliderExample">
       <Slider />
+    </Example>
+  );
+}
+function PageTransitionExample() {
+  return (
+    <Example title="PageTransitionExample">
       <Link to={`/${Date.now()}`}>move to random page</Link>
+    </Example>
+  );
+}
+
+function Example({ title, children }: { title: string; children: ReactNode }) {
+  return (
+    <div style={exampleContainerStyle}>
+      <h1 style={exampleTitleStyle}>{title}</h1>
+      {children}
     </div>
   );
 }
-const homeContainerStyle: CSSProperties = {
+const exampleContainerStyle: CSSProperties = {
   display: "flex",
-  flex: 1,
   flexDirection: "column",
   alignItems: "center",
-  justifyContent: "center",
   gap: 10,
-  overflow: "hidden",
+};
+const exampleTitleStyle: CSSProperties = {
+  fontSize: 20,
+  fontWeight: 700,
 };
